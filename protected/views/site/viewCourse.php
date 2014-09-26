f0b904537dc8dd09426cfae989c7d6669b03ff1e
Pavel Prohorov <redperchic@mail.ru>
2014-09-26 14:34:44 +0700

    Вывод учебных материалов + merged

--- protected/views/site/viewCourse.php
+++ protected/views/site/viewCourse.php
@@ -10,39 +10,39 @@
 ?>
 
 <?php
-    $this->renderPartial('top');
+$this->renderPartial('top');
 ?>
 
 
 <?php
-    $autors = CoursesAutor::model()->findAll('idCourse = '.$model->id);
-    $teachers = Course::getAutors($model->id);
-    $controlMaterials = CoursesControlMaterial::getAccessedControlMaterials($model->id);
+$autors = CoursesAutor::model()->findAll('idCourse = '.$model->id);
+$teachers = Course::getAutors($model->id);
+$controlMaterials = CoursesControlMaterial::getAccessedControlMaterials($model->id);
 ?>
 <div class="wrapper">
-<div class="container">
-<div class="col-group">
-<div class="col-9">
+    <div class="container">
+    <div class="col-group">
+    <div class="col-9">
 
-    <div class="content">
-        <div class="page-heading">
-            <div class="page-title">Курс: <?php echo $model->description?></div>
-            <div class="page-subtitle">Преподаватели:
-            <?php
-            $arTeachers = array();
-        	foreach ($teachers as $teacher){
-                array_push($arTeachers, "<a href = '#'>".$teacher->fio."</a>");
-            }
-            
-            echo implode(', ', $arTeachers);
-            ?>
+        <div class="content">
+            <div class="page-heading">
+                <div class="page-title">Курс: <?php echo $model->description?></div>
+                <div class="page-subtitle">Преподаватели:
+                    <?php
+                    $arTeachers = array();
+                    foreach ($teachers as $teacher){
+                        array_push($arTeachers, "<a href = '#'>".$teacher->fio."</a>");
+                    }
+
+                    echo implode(', ', $arTeachers);
+                    ?>
+                </div>
             </div>
-        </div>
-        
-        
-        <h2>Контрольные материалы</h2>
-        <table class="table green">
-		    <thead>
+
+
+            <h2>Контрольные материалы</h2>
+            <table class="table green">
+                <thead>
                 <tr>
                     <th>№</th>
                     <th width="40%" class="left">Название</th>
@@ -51,8 +51,8 @@
                     <th>Попыток</th>
                     <th width="20%">Дата</th>
                 </tr>
-            </thead>
-            <tbody>
+                </thead>
+                <tbody>
                 <?php $num = 0;?>
                 <?php foreach ($controlMaterials as $item):?>
                     <tr data-href = "<?php echo "/controlMaterial/startTest?idTest=".$item->id?>">
@@ -60,115 +60,109 @@
                         <td class="center"><?php echo $num; ?></td>
                         <td><?php echo $item->title ?></td>
                         <?php
-                            if ($item->question_show_count == -1) $showCount = count(Question::getQuestionsByControlMaterial($item->id)); else $showCount = $mat->question_show_count;
+                        if ($item->question_show_count == -1) $showCount = count(Question::getQuestionsByControlMaterial($item->id)); else $showCount = $mat->question_show_count;
                         ?>
                         <td class="center"><?php echo $showCount ?></td>
                         <td class="center"><?php echo $item->dotime?></td>
                         <?php
-                            $tries = UserControlMaterial::model()->findAll('idUser = :idUser and idControlMaterial = :idControlMaterial', array(':idUser' => Yii::app()->user->getId(), ':idControlMaterial' => $item->id));
-                            $countTries = count($tries);
+                        $tries = UserControlMaterial::model()->findAll('idUser = :idUser and idControlMaterial = :idControlMaterial', array(':idUser' => Yii::app()->user->getId(), ':idControlMaterial' => $item->id));
+                        $countTries = count($tries);
                         ?>
                         <td class="center"><?php echo $countTries?>/<?php echo ($item->try_amount == -1?'Неограниченно':$item->try_amount)?></td>
                         <?php
-                            $access = AccessControlMaterialGroup::model()->find('idControlMaterial = :idControlMaterial AND idGroup = NULL', array(':idControlMaterial' => $item->id));
-                            if ($access == null)
+                        $access = AccessControlMaterialGroup::model()->find('idControlMaterial = :idControlMaterial AND idGroup = NULL', array(':idControlMaterial' => $item->id));
+                        if ($access == null)
+                        {
+                            $accessText = "Открыт";
+                        } else
+                        {
+                            if ($access->access == 1) $accessText = "Открыт";
+                            if ($access->access == 2) $accessText = "Открыт";
+                            if ($access->access == 3)
                             {
                                 $accessText = "Открыт";
-                            } else
-                            {
-                                if ($access->access == 1) $accessText = "Открыт";
-                                if ($access->access == 2) $accessText = "Открыт";
-                                if ($access->access == 3)
+                                if ($accessText->startDate != '0000-00-00 00:00:00')
+                                    $accessText.= " с "+$accessText->startDate;
+                                if ($accessText->endDate != '0000-00-00 00:00:00')
                                 {
-                                    $accessText = "Открыт";
-                                } else
-                                {
-                                    if ($access->access == 1) $accessText = "Открыт";
-                                    if ($access->access == 2) $accessText = "Открыт";
-                                    if ($access->access == 3)
-                                    {
-                                        $accessText = "Открыт";
-                                        if ($accessText->startDate != '0000-00-00 00:00:00')
-                                            $accessText.= " с "+$accessText->startDate;
-                                        if ($accessText->endDate != '0000-00-00 00:00:00')
-                                        {
-                                            if ($accessText->startDate != '0000-00-00 00:00:00')
-                                                $accessText.= "<br>";
-                                            $accessText.= "до "+$accessText->endDate;
-                                        }
-                                    }
-                                    if ($access->access == 4) $accessText = "После предыдущего";
+                                    if ($accessText->startDate != '0000-00-00 00:00:00')
+                                        $accessText.= "<br>";
+                                    $accessText.= "до "+$accessText->endDate;
                                 }
-                                if ($access->access == 4) $accessText = "После предыдущего";
                             }
+                            if ($access->access == 4) $accessText = "После предыдущего";
+                        }
                         ?>
                         <td class="right"><?php echo $accessText ?></td>
                     </tr>
-                    <?
-                 endforeach ?>
+                <?
+                endforeach ?>
                 </tbody>
             </table>
 
-		</table>
-                        
-        <h2>Учебные материалы</h2>
-        <table class="table green">
-		    <thead>
-    		    <tr>
-        		    <th>№</th>
-        		    <th width="40%" class="left">Название</th>
-        		    <th>Расширение</th>
-        		    <th>Размер, КБ</th>
-        		    <th></th>
-    		    </tr>
-		    </thead>
-    		<tbody>
-        		<tr>
-            		<td class="center">1</td>
-            		<td>ВССиТ(2) Вопросы к экзамену</td>
-            		<td class="center">.doc</td>
-            		<td class="center">15</td>
-            		<td class="right"><a href="#">Скачать</a></td>
-        		</tr>
-        		<tr>
-            		<td class="title" colspan="5">
-            		    Лабораторные работы
-            		</td>
-        		</tr>
-        		<tr>
-            		<td class="center">1</td>
-            		<td>ВССиТ(2) Вопросы к экзамену</td>
-            		<td class="center">.doc</td>
-            		<td class="center">15</td>
-            		<td class="right"><a href="#">Скачать</a></td>
-        		</tr>
-        		<tr>
-            		<td class="center">1</td>
-            		<td>ВССиТ(2) Вопросы к экзамену</td>
-            		<td class="center">.doc</td>
-            		<td class="center">15</td>
-            		<td class="right"><a href="#">Скачать</a></td>
-        		</tr>
-        		<tr>
-            		<td class="center">1</td>
-            		<td>ВССиТ(2) Вопросы к экзамену</td>
-            		<td class="center">.doc</td>
-            		<td class="center">15</td>
-            		<td class="right"><a href="#">Скачать</a></td>
-        		</tr>
-        		<tr>
-            		<td class="center">1</td>
-            		<td>ВССиТ(2) Вопросы к экзамену</td>
-            		<td class="center">.doc</td>
-            		<td class="center">15</td>
-            		<td class="right"><a href="#">Скачать</a></td>
-        		</tr>
-    		</tbody>
-		</table>
-		
-    </div>
+            </table>
 
-</div>
+            <h2>Учебные материалы</h2>
+            <table class="table green">
+                <thead>
+                <tr>
+                    <th>№</th>
+                    <th width="40%" class="left">Название</th>
+                    <th>Расширение</th>
+                    <th>Размер </th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php
+                $i = 1;
+                $learnMaterials = LearnMaterial::getMaterialsFromCourse($model->id);
+                ?>
+                <?php foreach ($learnMaterials as $item):?>
+                    <tr>
+                        <?php if ($item->category == MATERIAL_TITLE):?>
+                            <td class="title" colspan="5">
+                                <?php echo $item->title; ?>
+                            </td>
+                        <? else: ?>
+                            <td class="center"><?php echo $i; ?></td>
+                            <td><?php echo $item->title; ?></td>
+                            <td class="center">
+                                <?php $path = "" ?>
+                                <?php if ($item->category==MATERIAL_FILE) $path = pathinfo($item->path, PATHINFO_EXTENSION)?>
+                                <?php echo $path?>
+                            </td>
+                            <td class="center">
+                                <?php
+                                $sizeText = "";
+                                if ($item->category == MATERIAL_FILE)
+                                {
+                                    $size = filesize($item->getPathToMaterial());
+                                    $sizePrefixxes = array(" Б"," Кб", " Мб", " Гб");
+                                    $i = 0;
+                                    do
+                                    {
+                                        $sizeText = $size.$sizePrefixxes[$i];
+                                        $i += 1;
+                                        $size = floor($size/1024);
+                                    } while ($size>0);
+                                }
+                                echo $sizeText;
+                                ?>
+
+                            </td>
+                            <td class="right"><a href="<?php echo $this->createUrl("/learnMaterial/getMaterial", array("matId" => $item->id)) ?>">Скачать</a></td>
+                            <?php $i++; ?>
+                        <? endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+                <tr>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+
+    </div>
 <?php
 $this->renderPartial("/site/bottom");
 ?>
