1636232709e15935aa4365a6cd00753a7d6f7538
Ilya Medvedev <ilya@medvedev.im>
2015-02-06 13:15:55 +0600

    Стилизация настроек

--- protected/views/site/userConfig/_form.php
+++ protected/views/site/userConfig/_form.php
@@ -4,7 +4,7 @@
 /* @var $form CActiveForm */
 ?>
 
-<div class="form">
+<div class="form wide">
 
 <?php $form=$this->beginWidget('CActiveForm', array(
     'htmlOptions' => array('enctype' => 'multipart/form-data'),
@@ -15,43 +15,69 @@
 	// See class documentation of CActiveForm for details on this.
 	'enableAjaxValidation'=>false,
 )); ?>
+    <style>
+    .input-full-width input {
+        width: 100%;
+    }
 
-	<div class="row">
-		<?php echo $form->labelEx($model,'login'); ?>
-		<?php echo $form->textField($model,'login',array('size'=>45,'maxlength'=>45)); ?>
-		<?php echo $form->error($model,'login'); ?>
-	</div>
+    .profile-avatar {
+        max-width: 100%;
+        height: auto;
+        border-radius: 50%;
+    }
+    </style>
+    <table width="80%">
+        <tr>
+            <td width="35%">
+                <?php echo $form->labelEx($model,'login'); ?>
+            </td>
+            <td width="65%" class="input-full-width">
+                <?php echo $form->textField($model,'login',array('size'=>45,'maxlength'=>45)); ?>
+                <?php echo $form->error($model,'login'); ?>
+            </td>
+        </tr>
+        <tr>
+            <td width="35%">
+                <?php echo CHTML::label("Новый пароль", "haveNewPassword"); ?>
+                <?php echo CHTML::checkBox("haveNewPassword",false, array('onclick' => 'checkHasNewPassword()')); ?>
+            </td>
+            <td width="65%" class="input-full-width">
+                <?php echo CHTML::passwordField("newPassword","",array("disabled" => true)); ?>
+            </td>
+        </tr>
+        <tr>
+            <td width="35%">
+                <?php echo $form->labelEx($model,'fio'); ?>
+                <?php echo $form->error($model,'fio'); ?>
+            </td>
+            <td width="65%" class="input-full-width">
+                <?php echo $form->textField($model,'fio',array('size'=>60,'maxlength'=>100)); ?>
+            </td>
+        </tr>
+        <tr>
+            <td width="35%">
+                <?php if ($model->avatar != ""):?>
+                    <img src = "/avatars/<?php echo $model->avatar?>" class="profile-avatar">
+                <?php else: ?>
+                    <img src = "/img/avatar-default.png" class="profile-avatar">
+                <?php endif; ?>
+                <?php //echo $form->labelEx($model,'avatar'); ?>
+                <?php echo $form->error($model,'avatar'); ?>
+            </td>
+            <td width="65%" class="input-full-width">
+                <?php echo $form->fileField($model,'newAvatar'); ?>
+                <?php if (!$model->isAvatarModerated):?>
+                    <br>
+                    <small>Ваш аватар еще не прошел модерацию администратором и поэтому не виден пользователям</small>
+                <?php endif;?>
+            </td>
+        </tr>
+    </table>
 
-    <div class="row">
-        <?php echo CHTML::label("Новый пароль", "haveNewPassword"); ?>
-        <?php echo CHTML::checkBox("haveNewPassword",false, array('onclick' => 'checkHasNewPassword()')); ?>
-        <?php echo CHTML::passwordField("newPassword","",array("disabled" => true)); ?>
-    </div>
-
-	<div class="row">
-		<?php echo $form->labelEx($model,'fio'); ?>
-		<?php echo $form->textField($model,'fio',array('size'=>60,'maxlength'=>100)); ?>
-		<?php echo $form->error($model,'fio'); ?>
-	</div>
-
-    <div class="row">
-        <?php echo $form->labelEx($model,'avatar'); ?>
-        <?php if ($model->avatar != ""):?>
-            <img src = "/avatars/<?php echo $model->avatar?>" class="profile-avatar">
-        <?php else: ?>
-            <img src = "/img/avatar-default.png" class="profile-avatar">
-        <?php endif; ?>
-        <?php echo $form->fileField($model,'newAvatar'); ?>
-        <?php echo $form->error($model,'avatar'); ?>
-
-        <?php if (!$model->isAvatarModerated):?>
-            <br>
-            Ваш аватар еще не прошел модерацию администратором и поэтому не виден пользователям
-        <?php endif;?>
-    </div>
+    <br>
 
 	<div class="row buttons">
-		<?php echo CHtml::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить'); ?>
+		<?php echo CHtml::submitButton($model->isNewRecord ? 'Создать' : 'Сохранить', array("class" => "btn blue")); ?>
 	</div>
 
 <?php $this->endWidget(); ?>
