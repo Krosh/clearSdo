307f88fee326f89ac5f4e15d9e7b94a59e0da1c3
Pavel Prohorov <redperchic@mail.ru>
2015-08-09 00:58:04 +0600

    Конференции в сообщениях

--- protected/views/message/dialogs.php
+++ protected/views/message/dialogs.php
@@ -8,16 +8,31 @@
  */?>
 
 <?php foreach ($items as $item):?>
-    <div class="dialog <?php if ($item["hasNonReadable"]) echo "unread" ?>" onclick="getDialogWithUser(<?php echo $item["user"]->id; ?>)" data-idUser = "<?php echo $item["user"]->id; ?>"">
+    <div class="dialog <?php if ($item["hasNonReadable"]) echo "unread" ?>" data-idUser = "<?php echo $item["idUser"]; ?>" data-isConf = "<?php echo $item['isConf']; ?>">
         <div class="row">
             <div class="col-xs-3">
-                <a href="#" class="avatar" style="background-image: url(<?php echo $item["user"]->getAvatarPath(AVATAR_SIZE_MEDIUM); ?>)"></a>
+                <a href="#" class="avatar" style="background-image: url(<?php echo (!$item['isConf']) ? $item["user"]->getAvatarPath(AVATAR_SIZE_MEDIUM) : "" ?>)"></a>
                 <span class="new">НОВОЕ</span>
             </div>
             <div class="col-xs-9">
-                <a href="#"><?php echo $item["user"]->getShortFio(); ?></a>
-                <p style="text-overflow: ellipsis; overflow:hidden; height: 35px">
-                    <span class="preview"><?php echo $item["message"]->text; ?></span>
+                <?php
+                    if ($item['isConf'])
+                    {
+                        $fioText = "";
+                        foreach ($item["user"] as $user)
+                        {
+                            if ($fioText != "")
+                                $fioText.= ", ";
+                            $fioText.= $user->getShortFio();
+                        }
+                    } else
+                    {
+                        $fioText = $item["user"]->getShortFio();
+                    }
+                ?>
+                <p style="text-overflow: ellipsis; overflow:hidden; height: 35px"><a href="#"><?php echo $fioText; ?></a></p>
+                <p >
+                    <span class="preview" style="text-overflow: ellipsis; overflow:hidden; height: 35px; display: inline-block"><?php echo $item["message"]->text; ?></span>
                     <br>
                     <span class="date"><?php if ($item["message"] != null) echo DateHelper::getDifference($item["message"]->dateSend,date("Y-m-d H:i:s")); ?></span>
                 </p>
